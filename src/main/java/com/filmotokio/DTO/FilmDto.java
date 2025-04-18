0bdc24653c61ba175b23965b06e490ffc3059a94
Balduino Mendes <baldhuino@gmail.com>
2025-04-18 14:49:40 +0100

    Film DTO

---
+++ src/main/java/com/filmotokio/DTO/FilmDto.java
@@ -0,0 +1,9 @@
+package com.filmotokio.DTO;
+
+public class FilmDto {
+    private String title;
+    private int year;
+    private int duration;
+    private String synopsis;
+    private String poster;
+}
