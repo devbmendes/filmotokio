192c28f0f422effe1b7024df438e8b1a62c094fd
Balduino Mendes <baldhuino@gmail.com>
2025-04-25 02:02:55 +0100

    LOGIN page and font changed

--- src/main/java/com/filmotokio/controller/AuthController.java
+++ src/main/java/com/filmotokio/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.filmotokio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class AuthController {
     @GetMapping
     public String home(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
     }
 }
--- src/main/java/com/filmotokio/controller/CastController.java
+++ src/main/java/com/filmotokio/controller/CastController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,6 +25,6 @@ public class CastController {
     @PostMapping("/save")
     public String save(CastDto castDto){
         castImpl.save(castDto);
-        return "";
+        return "redirect:/cast";
     }
 }
