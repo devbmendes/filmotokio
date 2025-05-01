179314783bf86a6a6904d9e05605be3ad3146d43
Balduino Mendes <baldhuino@gmail.com>
2025-05-01 18:37:03 +0100

    url changed for login and register

--- src/main/java/com/filmotokio/controller/AuthController.java
+++ src/main/java/com/filmotokio/controller/AuthController.java
@@ -1,13 +1,25 @@
 package com.filmotokio.controller;
 
+import com.filmotokio.DTO.UserDto;
+import com.filmotokio.service.UserImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+
 @Controller
-@RequestMapping
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    UserImpl userImpl;
 
     @GetMapping
     public String home(){
@@ -17,5 +29,26 @@ public class AuthController {
     @GetMapping("/login")
     public String login(){
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String user(Model model){
+
+        model.addAttribute("user",new UserDto());
+        return "userAdd";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("user") @Valid UserDto userDto, BindingResult result) throws IOException {
+        if(result.hasErrors()){
+            return "userAdd";
+        }
+        if (!userDto.getPassword().equals(userDto.getPasswordMatch())) {
+            result.rejectValue("passwordMatch", null, "As password não coincidem");
+            return "userAdd";
+        }
+        userImpl.save(userDto);
+
+        return "redirect:/auth/login";
     }
 }
--- src/main/java/com/filmotokio/controller/UserController.java
+++ src/main/java/com/filmotokio/controller/UserController.java
@@ -7,12 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,30 +19,12 @@ public class UserController {
     @Autowired
     private UserImpl userImpl;
 
-    @GetMapping
-    public String user(Model model){
 
-        model.addAttribute("user",new UserDto());
-        return "userAdd";
-    }
     @GetMapping("/all")
     public String getAll(Model model){
         List<User> users = userImpl.getAll();
         model.addAttribute("user",users);
         return "userList";
-    }
-    @PostMapping("/save")
-    public String saveUser(@ModelAttribute("user") @Valid UserDto userDto, BindingResult result) throws IOException {
-        if(result.hasErrors()){
-            return "userAdd";
-        }
-        if (!userDto.getPassword().equals(userDto.getPasswordMatch())) {
-            result.rejectValue("passwordMatch", null, "As password não coincidem");
-            return "userAdd";
-        }
-        userImpl.save(userDto);
-
-        return "redirect:/";
     }
     @PostMapping("/delete/{id}")
     public String deleteById(@PathVariable Long id, RedirectAttributes redirectAttributes){
