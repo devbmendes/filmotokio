3f10c561086868031ead8f27b3220bb4f7fc62a4
Balduino Mendes <baldhuino@gmail.com>
2025-05-03 18:23:33 +0100

    MyUserDetailsService

---
+++ src/main/java/com/filmotokio/service/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.filmotokio.service;
+
+import com.filmotokio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       com.filmotokio.model.User user = userRepository.findByEmail(username).orElseThrow(
+                ()-> new UsernameNotFoundException("Utilizador nao encontrado"));
+        return new User(user.getEmail(),user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_"+ user.getRole().name())));
+    }
+}
