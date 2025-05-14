df12666ec41eaa91d8bf0fc5b4d5b4b7ed06a0a3
Balduino Mendes <baldhuino@gmail.com>
2025-05-14 17:20:33 +0100

    RowMapper

---
+++ src/main/java/com/filmotokio/batch/mapper/FilmRowMapper.java
@@ -0,0 +1,27 @@
+package com.filmotokio.batch.mapper;
+
+import com.filmotokio.model.Film;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class FilmRowMapper implements RowMapper<Film> {
+
+    @Override
+    public Film mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Film film = new Film();
+        film.setId(rs.getLong("id"));
+        film.setTitle(rs.getString("title"));
+        film.setYear(rs.getInt("year"));
+        film.setDuration(rs.getInt("duration"));
+        film.setSynopsis(rs.getString("synopsis"));
+        film.setPoster(rs.getString("poster"));
+        
+        // Tratamento para LocalDate
+        film.setCreatedAt(rs.getObject("createdAt", LocalDate.class));
+        // Tratamento para migration que pode ser nulo
+        film.setMigration(rs.getObject("migration", LocalDate.class));
+        return film;
+    }
+}
